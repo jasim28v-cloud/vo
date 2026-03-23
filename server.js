d74ae144cdbac0b47598676282a3da5919db9a2e
jasim28v-cloud <jasim28v@gmail.com>
2026-03-23 10:14:52 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,23 @@
 const express = require('express');
+const compression = require('compression');
 const cors = require('cors');
 const path = require('path');
 const app = express();
 
-// إعدادات المنفذ (Port) ليتناسب مع Render تلقائياً
 const PORT = process.env.PORT || 3000;
 
-// تفعيل مشاركة الموارد وتنسيق البيانات
+app.use(compression()); 
 app.use(cors());
 app.use(express.json());
-
-// تشغيل ملفات الموقع (HTML, CSS, JS) من المجلد الرئيسي
 app.use(express.static(path.join(__dirname, '.')));
 
-// نقطة فحص للتأكد أن المحرك يعمل بنجاح
 app.get('/status', (req, res) => {
     res.json({ 
-        message: "Universal Engine is Live! 🚀",
-        status: "Running v99" 
+        message: "System Online",
+        speed: "Optimized" 
     });
 });
 
-// تشغيل السيرفر
 app.listen(PORT, () => {
-    console.log(Server is flying on port ${PORT} 🚀🌑);
+    console.log(System active on port ${PORT});
 });
